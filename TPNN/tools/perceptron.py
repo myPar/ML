668bb6f2f22790a156ed723fcfefd87a9189dbdc
Andrew <andrebobrovs@mail.ru>
2022-04-09 15:28:09 +0700

    lab2: implemented derivatives functions

--- TPNN/tools/perceptron.py
+++ TPNN/tools/perceptron.py
@@ -1,6 +1,8 @@
 import numpy as np
+import numpy.linalg as la
 
 
+# activation functions
 def ident(x):
     return x
 
@@ -20,12 +22,46 @@ def ELU(x, alpha):
         return alpha * (np.exp(x) - 1)
 
 
+# derivatives of activation functions
+def ident_der(x):
+    return 1
+
+
+def sigmoid_der(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+
+def th_der(x):
+    return 1 - th(x) ** 2
+
+
+def ELU_der(x, alpha):
+    if x >= 0:
+        return 1
+    else:
+        return alpha * np.exp(x)
+
+
+def get_der(function_ident):
+    if function_ident == sigmoid:
+        return sigmoid_der
+    elif function_ident == ident:
+        return ident_der
+    elif function_ident == th:
+        return th_der
+    elif function_ident == ELU:
+        return ELU_der
+    else:
+        assert False
+
+
 class Layer(object):
     def __init__(self, neuron_count, act_function):
         assert neuron_count > 0
 
         self.neuron_count = neuron_count  # count of neurons in layer
         self.activations = np.array([np.zeros(neuron_count)])  # fill activations with zeroes
+        self.z_array = np.array([np.zeros(neuron_count)])
         self.act_function = act_function
         self.weights = None  # weight matrix (Wij - weigh from i'th neuron of k-1 layer to
         ## j-th neuron of k layer)
@@ -108,10 +144,44 @@ class Net(object):
 
             z = np.matmul(w_matrix.transpose(), prev_layer_activations.transpose()) + cur_layer.biases.transpose()
             z = z.transpose()
+            cur_layer.z_array = z
+
             cur_layer.activations = np.apply_along_axis(cur_layer.act_function, 0, z)
         # output is last layer's activations:
         return self.layers[self.layers_count - 1].activations
 
+    # ------------------------#
+    # stochastic gradient descent methods:
+
+    # derivative of cost function on activation
+    def der_cost_act(self, layer_idx, neuron_idx, next_layer_act_der):
+        assert 0 <= layer_idx < self.layers_count - 1 and "out of bounds layer idx"
+        next_layer = self.layers[layer_idx + 1]
+        result = 0
+
+        for i in range(next_layer.neuron_count):
+            item = next_layer_act_der[i]
+            result += (item * get_der(next_layer.act_function)(next_layer.zarray[i]) * next_layer.weights[neuron_idx][i])
+
+        return result
+
+    # derivative of const function on weigh
+    def der_cost_weigh(self, layer_idx, i, j, cost_act_der_value):
+        assert 0 < layer_idx < self.layers_count
+        cur_layer = self.layers[layer_idx]
+        prev_layer = self.layers[layer_idx - 1]
+
+        return cost_act_der_value * get_der(cur_layer.act_function)(cur_layer.z_array[j]) * prev_layer.activations[i]
+
+    # derivative of cost function on bias
+    def der_cost_bias(self, layer_idx, neuron_idx, cost_act_der_value):
+        assert 0 < layer_idx < self.layers_count
+        cur_layer = self.layers[layer_idx]
+
+        return cost_act_der_value * get_der(cur_layer.act_function)(cur_layer.z_array[neuron_idx])
+
+    # --------------------#
+    # other methods:
     def print_net_config(self):
         print("layers=" + str(self.layers_count))
 
@@ -120,3 +190,11 @@ class Net(object):
             print("[" + str(i) + "]")
             layer.print_layer_config()
             i += 1
+
+
+def predict_error(out, target):
+    return la.norm(out - target, 'fro')  # l2 norm
+
+
+def predict_error_der(out, target, neuron_idx):
+    return out[neuron_idx] - target[neuron_idx]
