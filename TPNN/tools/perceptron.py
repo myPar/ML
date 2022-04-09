lab2: net calculation fixed and tested

TPNN/tools/perceptron.py
@@ -29,7 +29,7 @@ class Layer(object):
         self.act_function = act_function
         self.weights = None  # weight matrix (Wij - weigh from i'th neuron of k-1 layer to
         ## j-th neuron of k layer)
-        self.biases = np.zeros(neuron_count)  # fill bias array with zeroes
+        self.biases = np.array([np.zeros(neuron_count)])  # fill bias array with zeroes
 
     def set_weights(self, weight_matrix):
         shape = weight_matrix.shape[1]
@@ -37,7 +37,7 @@ class Layer(object):
         self.weights = weight_matrix
 
     def set_bias(self, biases):
-        shape = len(biases)
+        shape = len(biases[0])
         assert shape == self.neuron_count and "invalid bias array shape - " + str(shape)
         self.biases = biases
 
@@ -45,6 +45,7 @@ class Layer(object):
         print(" size=" + str(self.neuron_count))
         print(" act_function=" + str(self.act_function))
         print(" activations=" + str(self.activations))
+        print(" biases=" + str(self.biases))
         print(" weights:")
         if self.weights is None:
             print("     empty", end='')
@@ -84,8 +85,9 @@ class Net(object):
         self.layers[layer_idx].set_weights(weights)
 
     def init_biases(self, layer_idx: int, biases):
-        bias_length = len(biases)
+        bias_length = len(biases[0])
         assert bias_length == self.layers[layer_idx].neuron_count and "invalid bias vector size - " + str()
+        self.layers[layer_idx].set_bias(biases)
 
     def calc_output(self, input_data):
         first_layer = self.layers[0]
@@ -104,8 +106,9 @@ class Net(object):
             w_matrix = cur_layer.weights
             prev_layer_activations = prev_layer.activations
 
-            z = np.matmul(w_matrix.transpose(), prev_layer_activations.transpose()) + cur_layer.biases
-            self.layers[idx].activations = np.apply_along_axis(cur_layer.act_function, 0, z)
+            z = np.matmul(w_matrix.transpose(), prev_layer_activations.transpose()) + cur_layer.biases.transpose()
+            z = z.transpose()
+            cur_layer.activations = np.apply_along_axis(cur_layer.act_function, 0, z)
         # output is last layer's activations:
         return self.layers[self.layers_count - 1].activations
 