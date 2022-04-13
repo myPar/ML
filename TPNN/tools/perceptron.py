lab2: checked algorithm and derivatives calculations it's ok. fixed cost calculation. Cost function is decreases, but metrics are bad. TODO - implement Adam optimizer

TPNN/tools/perceptron.py
@@ -12,7 +12,7 @@ def sigmoid(x):
 
 
 def th(x):
-    return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+    return np.tanh(x)
 
 
 def ELU(x, alpha):