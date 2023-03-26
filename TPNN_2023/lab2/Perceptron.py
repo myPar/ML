35e44ad73a743389a5caa3e0112aa3b25255a30b
myPar <andrebobrovs@mail.ru>
2023-03-26 19:18:51 +0700

    implemented net initialization, start implementing optimizer

--- TPNN_2023/lab2/Perceptron.py
+++ TPNN_2023/lab2/Perceptron.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
 import numpy as np
-from enum import IntEnum
+from enum import IntEnum, StrEnum
 
 from TPNN_2023.lab2.activation_functions import *
-from TPNN_2023.lab2.tools import print_matrix
+from TPNN_2023.lab2.tools import print_matrix, Optimizer
 
 
 class ConfigLevel(IntEnum):
@@ -12,32 +12,50 @@ class ConfigLevel(IntEnum):
     HIGH = 3        # add gradient info
 
 
+class LayerType(StrEnum):
+    DENSE = "Dense"
+    SOFTMAX = "Softmax"
+
+
 class Layer(ABC):
     @abstractmethod
     def calc_output(self, input_vector):
         pass
 
     @abstractmethod
-    def learn_step(self, input_data):   # returns gradient array for input vector
+    def learn_step(self, *input_data):   # returns gradient array for input vector
         pass
 
     @abstractmethod
     def print_layer_config(self, config_level: int):
         pass
 
+    @abstractmethod
+    def get_layer_type(self):
+        pass
+
+    @abstractmethod
+    def init_layer(self, init_strategy):
+        pass
+
+    @abstractmethod
+    def set_input_shape(self, input_shape):
+        pass
+
 
 class Dense(Layer):
     def __init__(self, neuron_count, act_function):
         self.neuron_count = neuron_count
+        self.input_shape = None
+        self.act_function = act_function
+
         self.input_vector = None
         self.activations = None
         self.z_array = None
-        self.act_function = act_function
 
         self.weight_matrix = None
         self.biases = None
 
-        self.z_grad_array = None
         self.weights_grad_matrix = None
         self.biases_grad_array = None
 
@@ -57,21 +75,20 @@ class Dense(Layer):
         return self.z_array
 
     # returns gradient array for input vector (z_array for the previous layer):
-    def learn_step(self, prev_layer_z_array):
-        assert self.z_grad_array is not None
-        assert prev_layer_z_array.shape == (self.weight_matrix.shape[1],)
+    def learn_step(self, z_grad_array):
+        assert z_grad_array is not None
 
         # calc activation gradient array:
         yacobian = np.diag(np.apply_along_axis(func1d=get_der(self.act_function), axis=0, arr=self.activations))
         # no need to transpose, yacobian is a diagonal matrix
-        act_grad_array = np.dot(yacobian, self.z_grad_array)
+        act_grad_array = np.dot(yacobian, z_grad_array)
 
         # calc biases grad array
         biases_grad_array = act_grad_array
 
         # calc weighs grad matrix
         arg1 = np.array([act_grad_array]).transpose()
-        arg2 = np.array([prev_layer_z_array])
+        arg2 = np.array([self.input_vector])
         weights_grad_matrix = np.dot(arg1, arg2)
 
         assert weights_grad_matrix.shape == self.weight_matrix.shape
@@ -101,6 +118,22 @@ class Dense(Layer):
             print("biases gradient:")
             print_matrix(self.biases_grad_array, offset="  ")
 
+    def get_layer_type(self):
+        return LayerType.DENSE
+
+    def set_input_shape(self, shape):
+        assert len(shape) == 1
+        self.input_shape = shape
+
+    def init_layer(self, init_strategy):
+        assert self.input_shape is not None
+        # init matrix based on in shape and neurons count
+        weight_matrix_shape = (self.neuron_count, self.input_shape[0])
+        biases_shape = (self.neuron_count,)
+
+        self.weight_matrix = init_strategy(weight_matrix_shape)
+        self.biases = init_strategy(biases_shape)
+
 
 class Softmax(Layer):
     def __init__(self, neuron_count):
@@ -123,19 +156,77 @@ class Softmax(Layer):
     def print_layer_config(self, config_level: int):
         pass
 
+    def get_layer_type(self):
+        return LayerType.SOFTMAX
+
+    def init_layer(self, init_strategy):
+        pass
+
+    def set_input_shape(self, input_shape):
+        pass
+
 
 class Net:
     def __init__(self):
         self.layers = []
+        self.layers_count = 0
 
     def add_layer(self, layer: Layer):
         self.layers.append(layer)
+        self.layers_count += 1
 
-    def train_step(self, input_vector, target_vector):
-        pass
+    def train_step(self, input_vector, target_vector):  # return loss value for the sample
+        in_v = input_vector
 
-    def train(self, input_train_data, target_train_data, epoch_count: int):
-        pass
+        # forward propagation
+        for layer in self.layers:
+            in_v = layer.calc_output(in_v)
+
+        last_idx = self.layers_count - 1
+        z_grad_array = None
+
+        for i in range(last_idx, -1, 0):
+            cur_layer = self.layers[i]
+
+            layer_type = cur_layer.get_layer_type()
+            if i == last_idx and layer_type == LayerType.SOFTMAX:   # last layer is softmax layer
+                z_grad_array = cur_layer.learn_step(target_vector)
+            elif layer_type == LayerType.DENSE:
+                z_grad_array = cur_layer.learn_step(z_grad_array)
+            else:
+                assert False
+
+    def train(self, input_train_data, target_train_data, optimizer: Optimizer, learning_rate: float, epoch_count: int):
+        samples_count = len(input_train_data)
+        assert samples_count == len(target_train_data)
+
+        for epoch in range(epoch_count):
+            for i in range(samples_count):
+                sample = input_train_data[i]
+                target_sample = target_train_data[i]
+
+                self.train_step(sample, target_sample)
 
     def calc_output(self, input_vector):
-        pass
+        in_v = input_vector
+
+        for layer in self.layers:
+            in_v = layer.calc_output(in_v)
+
+        return in_v
+
+    def init_net(self, init_strategy, input_shape):
+        assert len(input_shape) == 1
+
+        for i in range(len(self.layers)):
+            cur_layer = self.layers[i]
+
+            if i == 0:
+                assert cur_layer.get_layer_type == LayerType.DENSE
+                cur_layer.set_input_shape(input_shape)
+            else:
+                prev_layer = self.layers[i - 1]
+                cur_input_shape = (prev_layer.neuron_count,)
+
+                cur_layer.set_input_shape(cur_input_shape)
+            cur_layer.init_layer(init_strategy)
--- TPNN_2023/lab2/tools.py
+++ TPNN_2023/lab2/tools.py
@@ -1,3 +1,7 @@
+import numpy as np
+from abc import ABC, abstractmethod
+
+
 def print_matrix(matrix, offset):
     if len(matrix.shape) == 1:
         for i in matrix:
@@ -15,3 +19,38 @@ def print_matrix(matrix, offset):
         for i in range(len(matrix)):
             print(offset + "[" + str(i) + "]:")
             print_matrix(matrix[i], offset + "    ")
+
+
+# uniform distribution from the specified interval
+def uniform_init(interval: tuple, array_shape: tuple):
+    assert len(interval) == 2
+    max_val = interval[1]
+    min_val = interval[0]
+
+    delta = max_val - min_val
+    assert delta > 0
+
+    return min_val + np.random.rand(*array_shape) * delta
+
+
+def default_init(array_shape: tuple):
+    return uniform_init((-0.5, 0.5), array_shape)
+
+
+class Optimizer(ABC):
+    @abstractmethod
+    def get_coefficient(self, *net_parameters):  # returns gradient coefficient and updates optimizer parameters
+        pass
+
+
+class Adam(Optimizer):
+    def __init__(self):
+        self.p1 = 0.9
+        self.p2 = 0.99
+        self.epsilon = 0.000001
+
+        self.r_t = 0    # zero on the first step
+        self.s_t = 0
+
+    def get_coefficient(self, full_gradient_norm: float):
+        pass
